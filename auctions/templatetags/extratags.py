b6e298bfb1d54d124aba98bb3fa5ac63b7227044
kenzotmg <kenzotmg@hotmail.com>
2021-04-23 22:07:43 -0300

    added func to check for watchlist state

--- auctions/templatetags/extratags.py
+++ auctions/templatetags/extratags.py
@@ -1,5 +1,7 @@
 from django import template
 from ..models import User
+from ..models import Listing
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
@@ -7,3 +9,11 @@ register = template.Library()
 @register.filter(name='getUserFromId')
 def getUserFromId(id):
     return User.objects.get(id=id)
+
+@register.filter(name='getWatchStateFromListing')
+def getStateFromListing(listing,user_id):
+    try:
+        watchlist = listing.watchlist.get(user_id=user_id)
+        return True
+    except ObjectDoesNotExist:
+        return False
